d27f53c3ccce735f168fb568c52630743d2efa3c
Timothy <timothysuldayan1@gmail.com>
2024-07-23 16:49:34 -0700

    Changed url paths for frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
+  origin: process.env.FRONTEND_URL || 'https://master--jrautodetailing.netlify.app/', 
   optionsSuccessStatus: 200
 }));
 app.use(express.json());
